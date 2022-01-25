d95abba64eeed5eb1cf451dbe9c7a03953b77cf8
Andrew <76924117+andrewskyboss@users.noreply.github.com>
2022-01-25 14:11:55 +0000

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -132,3 +132,22 @@ show_slide = (i) => {
 
 window.addEventListener("onload", show_slide(index));
 
+window.onload = function() {
+
+	document.getElementById('confirmation-title').textContent = '';
+
+	document.getElementById('contact-us-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+		emailjs.sendForm('service_u9b5bpg', 'template_a26hqai', this)
+		.then(function() {
+			console.log('SUCCESS!');
+			$('.form-input').val('');
+			document.getElementById('confirmation-title').textContent = 'Thank You. I will be in touch with you';
+
+		}, function(error) {
+			console.log('FAILED...', error);
+			document.getElementById('confirmation-title').textContent = 'Sorry, something went wrong. Try Later.';
+		});
+	});
+};

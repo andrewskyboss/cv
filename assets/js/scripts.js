bee3cc5e3aa48b1f3ea8b03dd4727de563fd06b4
andrewskyboss <andrew.cybossky@gmail.com>
2022-09-18 17:44:37 +0000

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -112,16 +112,19 @@ show_slide = (i) => {
     images[i].style.display = "none";
   
   // remove the active class from the dot
-  for (i = 0; i < dots.length; i++) 
+  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
+  }
   
   // if index is greater than the amount of images (set it to zero)
-  if (index > images.length - 1) 
+  if (index > images.length - 1) {
     index = 0 ;
+  }
   
   // if index is less than zero (set it to the length of images)
-  if (index < 0)
+  if (index < 0) {
     index = images.length - 1;
+  }
 
   // only display the image that's next or previous
   images[index].style.display = "block";
